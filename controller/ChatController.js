baacac051b8d1c4f7f55ded7fa1636255e26732c
june <junearagoncillo@gmail.com>
2022-10-06 17:57:00 +0800

    modified

--- controller/ChatController.js
+++ controller/ChatController.js
@@ -16,11 +16,10 @@ const addChatRoom = async (participants, message) => {
 
 const sendMessage = async (req, res) => {
     let { participants, chatRoomID, message} = req.body
-
+    let chatroom
     if(chatRoomID.length == '0'){
-        const chatroom = await addChatRoom(participants, message)
-        if(!chatroom.success)
-        return res.status(500).json({ success: false, message: 'Error in sending message'})
+        chatroom = await addChatRoom(participants, message)
+        if(!chatroom.success) return res.status(500).json({ success: false, message: 'Error in sending message'})
 
         chatRoomID = chatroom.data._id
     }
@@ -36,21 +35,30 @@ const sendMessage = async (req, res) => {
     
     try {
         await newMessage.save();
-        await Chatroom.findOneAndUpdate({ _id: chatRoomID }, { lastMessage: message })
-        return res.status(200).json({ success: true, message: 'Message sent', data: newMessage})
+        if(chatRoomID.length != '0'){
+            chatroom = await Chatroom.findOneAndUpdate({ _id: chatRoomID }, { lastMessage: message }, { new: true}).lean()
+        }
+        chatroom.participants.map( user => {
+            if(user._id != req.user._id){
+                chatroom.user = user
+            }
+        })
+        chatroom.messages = []
+        delete chatroom.participants
+        return res.status(200).json({ success: true, message: 'Message sent', data: newMessage, chatroom})
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message})
     }
 }
 
 const getChatrooms = async (req, res) => {
-    const chatrooms = await Chatroom.find({ "participants._id": req.user._id })
+    const chatrooms = await Chatroom.find({ "participants._id": req.user._id }).sort([['updatedAt', -1]])
     let chats = [];
 
     chatrooms.map( (el) => {
         let chat = {
             _id: el._id,
-            message: el.lastMessage,
+            lastMessage: el.lastMessage,
             createdAt: el.createdAt,
             updatedAt: el.updatedAt
         }
@@ -67,7 +75,7 @@ const getChatrooms = async (req, res) => {
 
 
 const getMessages = async (req, res) => {
-    const messages = await Message.find({ chatroomID: req.params.chatroomid, $or: [ { sender: req.user._id }, { receiver: req.user._id } ] })
+    const messages = await Message.find({ chatroomID: req.params.chatroomid, $or: [ { sender: req.user._id }, { receiver: req.user._id } ] }).sort([['createdAt', -1]])
     return res.status(200).json({ success: true, data: messages })
 }
 
